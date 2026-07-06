48722892b619300034e6d086ce4660ebe888a73f
belhadjt <belhadjt@gmail.com>
2026-07-06 15:11:48 +0100

    feat:dasboard view

--- src/pages/DashboardView.jsx
+++ src/pages/DashboardView.jsx
@@ -3,19 +3,18 @@ import StatsGrid from '../components/StatsGrid'
 import RecentOrdersTable from '../components/RecentOrdersTable';
 import OrderStatusCard from '../components/OrderStatusCard';
 import TopProductsCard from '../components/TopProductsCard';
-import DashboardLayout from '../components/DashboardLayout';
 
 export default function DashboardView() {
     return (
 
-        <DashboardLayout>
+        
             <div className='flex flex-col gap-6 p-4 lg:p-8'>
 
                 <div className="space-y-6">
                     <StatsGrid />
                 </div>
 
-                <div className=" grid gap-6 xl:grid-cols-[1.1fr_0.9fr]">
+                <div className=" grid gap-4 xl:grid-cols-[1.1fr_0.9fr]">
                     <OrderStatusCard />
                     <TopProductsCard />
                 </div>
@@ -26,6 +25,6 @@ export default function DashboardView() {
                 </div>
                 
             </div>
-        </DashboardLayout>
+        
     );
 }
