3b6a540dcf6a5b4a9b1cd3cf63fcc2e742c6cadb
FaroukZakaria <faroukzakaria2003@gmail.com>
2026-07-12 23:42:28 +0300

    feat: add layout components for Cart, Orders, Products, Settings, Template, and Users with session handling

---
+++ src/components/SettingsLayout.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import Sidebar from "./sidebar/Sidebar";
+import MobileSidebar from "./sidebar/MobileSidebar";
+import Topbar from "./Topbar";
+import DashboardView from "../pages/DashboardView";
+import LoadingSpinner from "./LoadingSpinner";
+import axios from "/src/api/axios";
+import ProtectedRoute from "./ProtectedRoute";
+
+const SettingsLayout = () => {
+  const [isSessionLoading, setIsSessionLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchAdminSession = async () => {
+      try {
+        const response = await axios.get("/auth/me");
+        setUserData(response.data.user || null);
+      } catch (error) {
+        console.error("Error fetching admin data from API", error);
+      } finally {
+        setIsSessionLoading(false);
+      }
+    };
+
+    fetchAdminSession();
+  }, []);
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+        {isSessionLoading ? <LoadingSpinner message="Loading session data..." /> :
+        <>
+        <Sidebar />
+        <MobileSidebar open={open} setOpen={setOpen} />
+        <div className="dashboard-main bg-amazon-bg lg:pl-72">
+        <Topbar userData={userData} open={open} onMenuClick={() => setOpen((prev) => !prev)} />
+        <ProtectedRoute>
+            {/* YOUR COMPONENT HERE */}
+        </ProtectedRoute>
+        </div>
+        </>}
+    </>
+  );
+};
+
+export default SettingsLayout;
